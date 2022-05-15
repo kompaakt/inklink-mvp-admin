eb67961b4037346b0f533a568595af6001ab3db2
d.shestakov <d.shestakov@x5.ru>
2022-05-15 15:37:57 +0300

    -

--- upload-file/index.mjs
+++ upload-file/index.mjs
@@ -3,4 +3,35 @@ import multer from "multer";
 import Minio from 'minio'
 import { v4 as uuidv4 } from 'uuid'
 
-console.log('start')
+const minio = new Minio.Client({
+  endPoint: process.env.HOST,
+  port: parseInt(process.env.MINIO_PORT, 10),
+  useSSL: false,
+  accessKey: process.env.MINIO_USER,
+  secretKey: process.env.MINIO_PASSWORD
+});
+
+console.log("env:", process.env)
+
+const app = express();
+
+app.post("/upload", multer({storage: multer.memoryStorage()}).single("file"), function(request, response) {
+  const id = uuidv4()
+  console.log('run upload')
+  minio.putObject(BUCKET, id, request.file.buffer, {
+    fieldName: request.file.fieldname,
+    "content-type": request.file.mimetype,
+    "Mimetype": request.file.mimetype, "Cache-Control" : "max-age=86400"
+  }, function(error) {
+      if(error) {
+          console.log(error);
+          response.status(500).send({
+            error
+          });
+        }
+        response.send(request?.file?.id);
+  });
+});
+
+
+app.listen(3000)
